e34a99cea64e89334fdf2e1daab1db342b2e7ad8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-12-01 09:56:07 +0100

    fixed curl

--- trigger-build.sh
+++ trigger-build.sh
@@ -9,6 +9,6 @@ curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token " $TRAVIS_API_TOKEN \
+  -H "Authorization: token $TRAVIS_API_TOKEN" \
   -d "$body" \
 https://api.travis-ci.org/repo/obsidian-toaster%2Fgenerator-backend/requests

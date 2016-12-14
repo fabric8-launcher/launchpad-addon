84af37135c0995bf07aa246bb88cc4f023142d74
George Gastaldi <gegastaldi@gmail.com>
2016-12-14 07:44:22 -0200

    Changed root package name to org.obsidiantoaster

--- src/main/java/org/obsidiantoaster/generator/addon/ui/ObsidianInitializer.java
+++ src/main/java/org/obsidiantoaster/generator/addon/ui/ObsidianInitializer.java
@@ -5,7 +5,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 
-package io.obsidian.generator.addon.ui;
+package org.obsidiantoaster.generator.addon.ui;
 
 import java.net.URL;
 import java.util.Arrays;

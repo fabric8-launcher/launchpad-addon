5c43b0adfe4dfa8a9d1515802123ef661c76f150
George Gastaldi <gegastaldi@gmail.com>
2016-12-22 12:23:58 -0200

    Fixed test

--- src/test/java/org/obsidiantoaster/generator/config/ConfigValueProducerTest.java
+++ src/test/java/org/obsidiantoaster/generator/config/ConfigValueProducerTest.java
@@ -1,25 +1,25 @@
 package org.obsidiantoaster.generator.config;
 
+import javax.inject.Inject;
+
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.obsidiantoaster.generator.config.ConfigValue;
-import org.obsidiantoaster.generator.config.ConfigValueProducer;
-
-import javax.inject.Inject;
 
 @RunWith(CdiRunner.class)
 @AdditionalClasses(ConfigValueProducer.class)
-public class ConfigValueProducerTest {
+public class ConfigValueProducerTest
+{
 
-    @Inject
-    @ConfigValue("archetype.catalog")
-    private String archetypeCatalogLocation;
+   @Inject
+   @ConfigValue("ARCHETYPE_CATALOG")
+   private String archetypeCatalogLocation;
 
-    @Test
-    public void shouldInjectCatalogLocationUrl() {
-        Assert.assertNotNull(archetypeCatalogLocation);
-    }
+   @Test
+   public void shouldInjectCatalogLocationUrl()
+   {
+      Assert.assertNotNull(archetypeCatalogLocation);
+   }
 }

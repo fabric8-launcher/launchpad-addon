287c609fe27199d2c985c42bca427a20c227c16e
George Gastaldi <gegastaldi@gmail.com>
2017-03-30 19:18:30 +0100

    Ignore .obsidian dirs

--- src/main/java/io/openshift/launchpad/ui/quickstart/NewProjectWizard.java
+++ src/main/java/io/openshift/launchpad/ui/quickstart/NewProjectWizard.java
@@ -60,7 +60,7 @@ public class NewProjectWizard implements UIWizard
     * Files to be deleted after project creation (if exists)
     */
    private static final List<String> FILES_TO_BE_DELETED = Arrays.asList(".git", ".travis", ".travis.yml", ".ds_store",
-            ".openshiftio");
+            ".openshiftio", ".obsidian");
 
    @Inject
    @WithAttributes(label = "Booster", required = true)

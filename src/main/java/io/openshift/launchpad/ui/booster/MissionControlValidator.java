5b78ce4122381c27a1c70c5048a2a416fe7377e0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-08-31 15:42:47 +0200

    cache the result in the UIContext

--- src/main/java/io/openshift/launchpad/ui/booster/MissionControlValidator.java
+++ src/main/java/io/openshift/launchpad/ui/booster/MissionControlValidator.java
@@ -43,9 +43,11 @@ public class MissionControlValidator
    public List<String> getProjects(UIContext context)
    {
       Map<Object, Object> attributeMap = context.getAttributeMap();
-      List<String> authList = (List<String>) attributeMap.get(HttpHeaders.AUTHORIZATION);
-      String authHeader = (authList == null || authList.isEmpty()) ? null : authList.get(0);
-      return missionControlFacade.getProjects(authHeader);
+      return (List<String>) attributeMap.computeIfAbsent("projects", key -> {
+         List<String> authList = (List<String>) attributeMap.get(HttpHeaders.AUTHORIZATION);
+         String authHeader = (authList == null || authList.isEmpty()) ? null : authList.get(0);
+         return missionControlFacade.getProjects(authHeader);
+      });
    }
 
    public boolean validateGitHubTokenExists(UIValidationContext context)

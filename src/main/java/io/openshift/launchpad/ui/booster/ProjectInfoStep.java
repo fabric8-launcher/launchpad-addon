5ce575022db981adda01113ad4c7d3bd8485d6e4
George Gastaldi <gegastaldi@gmail.com>
2017-09-27 09:16:45 -0300

    Removed project list from Openshift Project name completers

--- src/main/java/io/openshift/launchpad/ui/booster/ProjectInfoStep.java
+++ src/main/java/io/openshift/launchpad/ui/booster/ProjectInfoStep.java
@@ -42,7 +42,6 @@ import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Categories;
-import org.jboss.forge.addon.ui.util.Completers;
 import org.jboss.forge.addon.ui.util.Metadata;
 import org.jboss.forge.addon.ui.wizard.UIWizardStep;
 import org.jboss.forge.furnace.util.Strings;
@@ -152,8 +151,6 @@ public class ProjectInfoStep implements UIWizardStep
    {
       if (deploymentType == DeploymentType.CD)
       {
-         String openShiftCluster = (String) builder.getUIContext().getAttributeMap().get("OPENSHIFT_CLUSTER");
-         named.setCompleter(Completers.fromValues(missionControlValidator.getProjects(builder.getUIContext(), openShiftCluster)));
          builder.add(named).add(gitHubRepositoryName);
       }
    }
